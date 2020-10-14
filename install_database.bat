4fdad63a01de771e84ac2e4eea98957552c05411
Thestickman391 <4157860+Thestickman391@users.noreply.github.com>
2020-10-14 14:33:24 +1300

    Update and rename reinstall_database.bat to install_database.bat

---
+++ install_database.bat
@@ -0,0 +1,3 @@
+echo !!! MAKE SURE YOU HAVE MYSQLD OPEN (MYSQL SERVER) WHEN EXECUTING THIS !!!
+mysql\bin\mysql --user=root --password= < mysql\create.sql
+mysql\bin\mysql --user=root --password= < mysql\tables.sql
--- reinstall_database.bat
+++
@@ -1,9 +0,0 @@
-for /D %%D in (".\mysql\data\*.*") do (
-    if /I not "%%~nxD"=="mysql" (
-        2> nul rd /S /Q "%%~fD"
-    )
-)
-del /Q .\mysql\data\*.*
-mysql\bin\mysqld --standalone --console
-mysql\bin\mysql --user=root --password= < mysql\create.sql
-mysql\bin\mysql --user=root --password= < mysql\tables.sql
